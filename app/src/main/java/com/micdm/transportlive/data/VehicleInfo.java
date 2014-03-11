c5f3374e8f82f492e368011060ab805d645ed966
Mic <mic-dm@mail.ru>
2014-03-11 16:12:10 +0700

    new: иконки для машин

---
+++ app/src/main/java/com/micdm/transportlive/data/VehicleInfo.java
@@ -0,0 +1,12 @@
+package com.micdm.transportlive.data;
+
+public class VehicleInfo {
+
+    public Route route;
+    public Vehicle vehicle;
+
+    public VehicleInfo(Route route, Vehicle vehicle) {
+        this.route = route;
+        this.vehicle = vehicle;
+    }
+}
--- app/src/main/java/com/micdm/transportlive/fragments/MapFragment.java
+++ app/src/main/java/com/micdm/transportlive/fragments/MapFragment.java
@@ -1,6 +1,15 @@
 package com.micdm.transportlive.fragments;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,11 +22,13 @@ import com.micdm.transportlive.data.Route;
 import com.micdm.transportlive.data.Service;
 import com.micdm.transportlive.data.Transport;
 import com.micdm.transportlive.data.Vehicle;
+import com.micdm.transportlive.data.VehicleInfo;
 import com.micdm.transportlive.misc.ServiceHandler;
 
 import org.apache.commons.io.IOUtils;
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.IRegisterReceiver;
 import org.osmdroid.tileprovider.MapTileProviderArray;
 import org.osmdroid.tileprovider.modules.IArchiveFile;
@@ -40,20 +51,81 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MapFragment extends Fragment {
+
+    private static class MarkerBuilder {
+
+        private Resources resources;
+        private Bitmap original;
+        private Paint paint;
+
+        public MarkerBuilder(Resources resources) {
+            this.resources = resources;
+            setupOriginalBitmap();
+            setupPaint();
+        }
+
+        private void setupOriginalBitmap() {
+            original = BitmapFactory.decodeResource(resources, R.drawable.vehicle);
+        }
+
+        private void setupPaint() {
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setTextSize(original.getWidth() / 2);
+        }
+
+        public OverlayItem[] build(VehicleInfo[] vehicles) {
+            ArrayList<OverlayItem> markers = new ArrayList<OverlayItem>();
+            for (VehicleInfo info: vehicles) {
+                markers.add(getMarker(info));
+            }
+            return markers.toArray(new OverlayItem[markers.size()]);
+        }
+
+        private OverlayItem getMarker(VehicleInfo info) {
+            OverlayItem marker = new OverlayItem(info.vehicle.number, "", new GeoPoint(info.vehicle.location.latitude, info.vehicle.location.longitude));
+            Bitmap bitmap = getBitmap(info);
+            marker.setMarker(new BitmapDrawable(resources, bitmap));
+            marker.setMarkerHotspot(OverlayItem.HotspotPlace.CENTER);
+            return marker;
+        }
+
+        private Bitmap getBitmap(VehicleInfo info) {
+            Bitmap result = Bitmap.createBitmap(original.getWidth(), original.getHeight(), original.getConfig());
+            Canvas canvas = new Canvas(result);
+            Matrix matrix = new Matrix();
+            matrix.setRotate(info.vehicle.direction, original.getWidth() / 2, original.getHeight() / 2);
+            canvas.drawBitmap(original, matrix, null);
+            String text = String.valueOf(info.route.number);
+            Rect bounds = getTextBounds(text);
+            canvas.drawText(text, canvas.getWidth() / 2 - bounds.width() / 2, canvas.getHeight() / 2 + bounds.height() / 2, paint);
+            return result;
+        }
+
+        private Rect getTextBounds(String text) {
+            Rect bounds = new Rect();
+            paint.getTextBounds(text, 0, text.length(), bounds);
+            return bounds;
+        }
+    }
 
     private static final String ATLAS_FILE_NAME = "atlas.zip";
     private static final int TILE_SIZE = 256;
     private static final int MIN_ZOOM = 14;
     private static final int MAX_ZOOM = 15;
-    private static final int NORTH = 56547372;
-    private static final int WEST = 84902344;
-    private static final int SOUTH = 56438204;
-    private static final int EAST = 85122070;
+    private static final int NORTH_EDGE = 56547372;
+    private static final int WEST_EDGE = 84902344;
+    private static final int SOUTH_EDGE = 56438204;
+    private static final int EAST_EDGE = 85122070;
+    private static final GeoPoint INITIAL_LOCATION = new GeoPoint(56484642, 84948100);
 
     private ServiceHandler handler;
+    private MarkerBuilder builder;
 
     @Override
     public void onAttach(Activity activity) {
@@ -65,18 +137,26 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, null);
         if (view != null) {
-            ResourceProxy proxy = new DefaultResourceProxyImpl(getActivity());
-            MapTileProviderArray providers = getTileProviders();
-            MapView mapView = new MapView(getActivity(), TILE_SIZE, proxy, providers);
-            mapView.setId(R.id.map);
-            mapView.setVisibility(View.GONE);
-            mapView.setMinZoomLevel(MIN_ZOOM);
-            mapView.setMaxZoomLevel(MAX_ZOOM);
-            mapView.setScrollableAreaLimit(new BoundingBoxE6(NORTH, EAST, SOUTH, WEST));
-            mapView.setMultiTouchControls(true);
+            MapView mapView = getMapView();
             ((ViewGroup) view).addView(mapView);
+            IMapController controller = mapView.getController();
+            controller.setCenter(INITIAL_LOCATION);
+            controller.setZoom(MAX_ZOOM);
         }
         return view;
+    }
+
+    private MapView getMapView() {
+        ResourceProxy proxy = new DefaultResourceProxyImpl(getActivity());
+        MapTileProviderArray providers = getTileProviders();
+        MapView mapView = new MapView(getActivity(), TILE_SIZE, proxy, providers);
+        mapView.setId(R.id.map);
+        mapView.setVisibility(View.GONE);
+        mapView.setMinZoomLevel(MIN_ZOOM);
+        mapView.setMaxZoomLevel(MAX_ZOOM);
+        mapView.setScrollableAreaLimit(new BoundingBoxE6(NORTH_EDGE, EAST_EDGE, SOUTH_EDGE, WEST_EDGE));
+        mapView.setMultiTouchControls(true);
+        return mapView;
     }
 
     private MapTileProviderArray getTileProviders() {
@@ -133,21 +213,22 @@ public class MapFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        handler.setOnUnselectAllRoutesListener(null);
         handler.setOnLoadVehiclesListener(null);
     }
 
     public void update(Service service) {
         hideViews();
-        Vehicle[] vehicles = getVehicles(service);
+        VehicleInfo[] vehicles = getVehicles(service);
         if (vehicles.length == 0) {
             showView(R.id.no_vehicles);
             return;
         }
         showView(R.id.map);
-        ArrayList<OverlayItem> markers = new ArrayList<OverlayItem>();
-        for (Vehicle vehicle: vehicles) {
-            markers.add(getMarker(vehicle));
+        if (builder == null) {
+            builder = new MarkerBuilder(getResources());
         }
+        OverlayItem[] markers = builder.build(vehicles);
         updateMap(markers);
     }
 
@@ -168,27 +249,33 @@ public class MapFragment extends Fragment {
         }
     }
 
-    private Vehicle[] getVehicles(Service service) {
-        ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+    private VehicleInfo[] getVehicles(Service service) {
+        ArrayList<VehicleInfo> vehicles = new ArrayList<VehicleInfo>();
         for (Transport transport: service.transports) {
             for (Route route: transport.routes) {
                 for (Direction direction: route.directions) {
-                    vehicles.addAll(direction.vehicles);
+                    for (Vehicle vehicle: direction.vehicles) {
+                        vehicles.add(new VehicleInfo(route, vehicle));
+                    }
                 }
             }
         }
-        return vehicles.toArray(new Vehicle[vehicles.size()]);
+        return vehicles.toArray(new VehicleInfo[vehicles.size()]);
     }
 
-    private OverlayItem getMarker(Vehicle vehicle) {
-        OverlayItem marker = new OverlayItem(vehicle.number, "", new GeoPoint(vehicle.location.latitude, vehicle.location.longitude));
-        marker.setMarker(getResources().getDrawable(android.R.drawable.arrow_down_float));
-        marker.setMarkerHotspot(OverlayItem.HotspotPlace.BOTTOM_CENTER);
-        return marker;
+    private void updateMap(OverlayItem[] markers) {
+        MapView view = (MapView) getView().findViewById(R.id.map);
+        if (view != null) {
+            List<Overlay> overlays = view.getOverlays();
+            overlays.clear();
+            overlays.add(getLayer(markers));
+        }
     }
 
-    private Overlay getLayer(List<OverlayItem> markers) {
-        return new ItemizedIconOverlay<OverlayItem>(getActivity(), markers, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+    private Overlay getLayer(OverlayItem[] markers) {
+        ArrayList<OverlayItem> list = new ArrayList<OverlayItem>();
+        Collections.addAll(list, markers);
+        return new ItemizedIconOverlay<OverlayItem>(getActivity(), list, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
             @Override
             public boolean onItemSingleTapUp(int i, OverlayItem item) {
                 return false;
@@ -198,14 +285,5 @@ public class MapFragment extends Fragment {
                 return false;
             }
         });
-    }
-
-    private void updateMap(List<OverlayItem> markers) {
-        MapView view = (MapView) getView().findViewById(R.id.map);
-        if (view != null) {
-            List<Overlay> overlays = view.getOverlays();
-            overlays.clear();
-            overlays.add(getLayer(markers));
-        }
     }
 }
