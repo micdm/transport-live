75a88b584721aaa50b0ca124bde4281695b6ee3e
Mic <mic-dm@mail.ru>
2014-04-27 16:15:24 +0700

    new: разрешаем несколько одновременных подключений + разрываем соединение, если пакет логина долго идет

--- server/src/transportlive/data/server.py
+++ server/src/transportlive/data/server.py
@@ -1,65 +1,77 @@
 # coding=utf-8
 
+from datetime import timedelta
 from logging import getLogger
 
+from tornado.ioloop import IOLoop
 from tornado.options import options
 from tornado.tcpserver import TCPServer
 
 from transportlive.data.packet_utils import PacketSerializer, PacketUnserializer, PacketBuilder
-from transportlive.data.packets import LoginPacket, LoginAnswerPacket, PingPacket, PingAnswerPacket, DataPacket, DataAnswerPacket
+from transportlive.data.packets import LoginPacket, LoginAnswerPacket, PingPacket, PingAnswerPacket, DataPacket,\
+    DataAnswerPacket
 from transportlive.vehicle_builder import VehicleBuilder
 
 logger = getLogger(__name__)
 
 class DataServer(TCPServer):
 
-    MAX_BUFFER_SIZE = 1024
-
     def __init__(self, datastore):
         super(DataServer, self).__init__()
+        self._vehicle_builder = VehicleBuilder()
+        self._datastore = datastore
+        self._stream_count = 0
+
+    def handle_stream(self, stream, address):
+        logger.info("New connection from %s:%s", *address)
+        self._stream_count += 1
+        StreamHandler(self._stream_count, stream, self._handle_data_packet).run()
+
+    def _handle_data_packet(self, packet):
+        vehicle = self._vehicle_builder.build(packet)
+        if not vehicle:
+            logger.info("Cannot build vehicle, skipping packet...")
+        else:
+            self._datastore.add_vehicle(vehicle)
+
+class StreamHandler(object):
+
+    MAX_BUFFER_SIZE = 1024
+    CLOSE_STREAM_INTERVAL = timedelta(seconds=30)
+
+    def __init__(self, handler_id, stream, on_data_packet):
         self._packet_serializer = PacketSerializer()
         self._packet_unserializer = PacketUnserializer()
         self._packet_builder = PacketBuilder()
-        self._vehicle_builder = VehicleBuilder()
-        self._stream = None
+        self._id = handler_id
+        self._stream = stream
         self._buffer = ""
         self._session = None
-        self._datastore = datastore
+        self._on_data_packet = on_data_packet
+        self._close_timeout = None
 
-    def handle_stream(self, stream, address):
-        # TODO: разрешать повторное подключение
-        # TODO: запускать таймер и обрывать соединение, если не пришел пакет логина
-        logger.info("New connection from %s:%s", *address)
-        if self._stream:
-            logger.warning("Cannot handle connection, there is another one active already")
-        else:
-            self._stream = stream
-            stream.read_until_close(lambda data: self._on_close_stream(), self._on_stream_data)
+    def run(self):
+        self._stream.read_until_close(lambda data: self._on_close_stream(), self._on_stream_data)
+        self._close_timeout = IOLoop.instance().add_timeout(self.CLOSE_STREAM_INTERVAL, self._close_unathorized_stream)
+
+    def _close_unathorized_stream(self):
+        logger.warning("Stream %s unathorized too long, closing stream...", self._id)
+        self._stream.close()
 
     def _on_stream_data(self, data):
-        logger.debug("New data on connection of length %s", len(data))
-        logger.debug(data)
+        logger.debug("New data on stream %s of length %s:\r\n%s", self._id, len(data), data)
         self._buffer += data
         self._parse_packets()
         if len(self._buffer) > self.MAX_BUFFER_SIZE:
-            logger.warning("Buffer grows too much, closing connection...")
+            logger.warning("Buffer grows too much, closing stream...")
             self._stream.close()
-
-    def _on_close_stream(self):
-        logger.info("Connection closed")
-        self._cleanup()
 
     def _parse_packets(self):
         while True:
             packet = self._parse_packet()
             if not packet:
                 break
-            if isinstance(packet, LoginPacket):
-                self._handle_login_packet(packet)
-            if isinstance(packet, PingPacket):
-                self._handle_ping_packet(packet)
-            if isinstance(packet, DataPacket):
-                self._handle_data_packet(packet)
+            self._handle_packet(packet)
 
     def _parse_packet(self):
         if not self._buffer:
@@ -71,20 +83,29 @@ class DataServer(TCPServer):
         self._buffer = self._buffer[length:]
         return self._packet_builder.build(packet_type, parts)
 
+    def _handle_packet(self, packet):
+        if isinstance(packet, LoginPacket):
+            self._handle_login_packet(packet)
+        if isinstance(packet, PingPacket):
+            self._handle_ping_packet(packet)
+        if isinstance(packet, DataPacket):
+            self._handle_data_packet(packet)
+
     def _handle_login_packet(self, packet):
         logger.info("Login packet received")
         if self._session:
-            logger.warning("Session already started, closing connection...")
+            logger.warning("Session already started, closing stream...")
             self._stream.close()
         else:
             if packet.login != options.DATA_SERVER["login"] or packet.password != options.DATA_SERVER["password"]:
-                logger.warning("Wrong login (%s) or password, closing connection...", packet.login)
+                logger.warning("Wrong login (%s) or password, closing stream...", packet.login)
                 self._stream.close()
             else:
                 logger.info("Starting new session...")
                 answer_packet = LoginAnswerPacket(LoginAnswerPacket.STATUS_OK)
                 self._stream.write(self._packet_serializer.serialize(answer_packet))
                 self._session = Session(packet.login)
+                IOLoop.instance().remove_timeout(self._close_timeout)
 
     def _handle_ping_packet(self, packet):
         logger.info("Ping packet received")
@@ -94,21 +115,15 @@ class DataServer(TCPServer):
     def _handle_data_packet(self, packet):
         logger.info("Data packet received")
         if not self._session:
-            logger.warning("No session started, closing connection...")
+            logger.warning("No session started, closing stream...")
             self._stream.close()
         else:
             answer_packet = DataAnswerPacket(DataAnswerPacket.STATUS_OK)
             self._stream.write(self._packet_serializer.serialize(answer_packet))
-            vehicle = self._vehicle_builder.build(packet)
-            if not vehicle:
-                logger.info("Cannot build vehicle, skipping packet...")
-            else:
-                self._datastore.add_vehicle(vehicle)
+            self._on_data_packet(packet)
 
-    def _cleanup(self):
-        self._stream = None
-        self._buffer = ""
-        self._session = None
+    def _on_close_stream(self):
+        logger.info("Stream %s closed", self._id)
 
 class Session(object):
 
